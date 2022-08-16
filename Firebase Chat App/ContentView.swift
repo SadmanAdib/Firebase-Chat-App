aaf3715278c1181f929cdd2fd2064fe253683966
Sadman Adib <sadmanadib67@gmail.com>
2022-08-16 09:50:49 +0600

    Initial Commit

---
+++ Firebase Chat App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Firebase Chat App
+//
+//  Created by Sadman Adib on 16/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Firebase Chat App/Firebase_Chat_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Firebase_Chat_AppApp.swift
+//  Firebase Chat App
+//
+//  Created by Sadman Adib on 16/8/22.
+//
+
+import SwiftUI
+
+@main
+struct Firebase_Chat_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
