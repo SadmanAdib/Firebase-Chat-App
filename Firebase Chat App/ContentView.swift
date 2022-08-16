a577dc07db8ea53e2bd4eb839f49b23ee89860c4
Sadman Adib <sadmanadib67@gmail.com>
2022-08-16 10:03:19 +0600

    Login/CreateAccount Page UI done

--- Firebase Chat App/ContentView.swift
+++ Firebase Chat App/ContentView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  LoginView.swift
 //  Firebase Chat App
 //
 //  Created by Sadman Adib on 16/8/22.
@@ -7,15 +7,81 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct LoginView: View {
+
+    @State var isLoginMode = false
+    @State var email = ""
+    @State var password = ""
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            ScrollView {
+
+                VStack(spacing: 16) {
+                    Picker(selection: $isLoginMode, label: Text("Picker here")) {
+                        Text("Login")
+                            .tag(true)
+                        Text("Create Account")
+                            .tag(false)
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+
+                    if !isLoginMode {
+                        Button {
+
+                        } label: {
+                            Image(systemName: "person.fill")
+                                .font(.system(size: 64))
+                                .padding()
+                        }
+                    }
+
+                    Group {
+                        TextField("Email", text: $email)
+                            .keyboardType(.emailAddress)
+                            .autocapitalization(.none)
+                        SecureField("Password", text: $password)
+                    }
+                    .padding(12)
+                    .background(Color.white)
+                    .cornerRadius(10)
+
+                    Button {
+                        handleAction()
+                    } label: {
+                        HStack {
+                            Spacer()
+                            Text(isLoginMode ? "Log In" : "Create Account")
+                                .foregroundColor(.white)
+                                .padding(.vertical, 10)
+                                .font(.system(size: 14, weight: .semibold))
+                            Spacer()
+                        }
+                        .background(Color.blue)
+                        .cornerRadius(10)
+
+                    }
+                }
+                .padding()
+
+            }
+            .navigationTitle(isLoginMode ? "Log In" : "Create Account")
+            .background(Color(.init(white: 0, alpha: 0.05))
+                            .ignoresSafeArea())
+        }
+    }
+
+    private func handleAction() {
+        if isLoginMode {
+            print("Logging in")
+        } else {
+            print("Creating Account")
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        LoginView()
     }
 }
