8e0c40f9cfa095463c00428a011ef3ab8de03462
Sadman Adib <sadmanadib67@gmail.com>
2022-08-21 12:36:17 +0600

    Refactored

--- Firebase Chat App/MainMessages/MainMessagesView.swift
+++ Firebase Chat App/MainMessages/MainMessagesView.swift
@@ -7,159 +7,21 @@
 
 import SwiftUI
 
-class MainMessagesViewModel: ObservableObject {
-
-    @Published private var errorMessage = ""
-    @Published var chatUser: ChatUser?
-    @Published var isUserCurrentlyLoggedOut = false
-
-    init() {
-        fetchCurrentUser()
-        
-        DispatchQueue.main.async {
-            self.isUserCurrentlyLoggedOut = FirebaseManager.shared.auth.currentUser?.uid == nil
-        }
-    }
-
-     func fetchCurrentUser() {
-
-        guard let uid = FirebaseManager.shared.auth.currentUser?.uid else {
-            self.errorMessage = "Could not find firebase uid"
-            return
-        }
-
-        FirebaseManager.shared.firestore.collection("users").document(uid).getDocument { snapshot, error in
-            if let error = error {
-                self.errorMessage = "Failed to fetch current user: \(error)"
-                print("Failed to fetch current user:", error)
-                return
-            }
-
-            guard let data = snapshot?.data() else {
-                self.errorMessage = "No data found"
-                return
-
-            }
-            
-            self.chatUser = .init(data: data)
-        }
-    }
-    
-    func handleSignOut() {
-        isUserCurrentlyLoggedOut.toggle()
-        try? FirebaseManager.shared.auth.signOut()
-    }
-
-}
-
 struct MainMessagesView: View {
     
     @State var shouldShowLogOutOptions = false
     @StateObject var vm = MainMessagesViewModel()
 
-        private var customNavBar: some View {
-            HStack(spacing: 16) {
-
-                AsyncImage(url: URL(string: vm.chatUser?.profileImageUrl ?? "")) { returnedImage in
-                    returnedImage
-                        .resizable()
-                        .scaledToFill()
-                        .frame(width: 50, height: 50)
-                        .clipped()
-                        .cornerRadius(50)
-                        .overlay(RoundedRectangle(cornerRadius: 44)
-                            .stroke(Color(.label), lineWidth: 1)
-                        )
-                        .shadow(radius: 5)
-                } placeholder: {
-                    ProgressView()
-                }
-                
-                VStack(alignment: .leading, spacing: 4) {
-                    Text(vm.chatUser?.email.replacingOccurrences(of: "@gmail.com", with: "") ?? "")
-                        .font(.system(size: 24, weight: .bold))
-
-                    HStack {
-                        Circle()
-                            .foregroundColor(.green)
-                            .frame(width: 14, height: 14)
-                        Text("online")
-                            .font(.system(size: 12))
-                            .foregroundColor(Color(.lightGray))
-                    }
-
-                }
-
-                Spacer()
-                Button {
-                    shouldShowLogOutOptions.toggle()
-                } label: {
-                    Image(systemName: "gear")
-                        .font(.system(size: 24, weight: .bold))
-                        .foregroundColor(Color(.label))
-                }
-            }
-            .padding()
-            .actionSheet(isPresented: $shouldShowLogOutOptions) {
-                .init(title: Text("Settings"), message: Text("What do you want to do?"), buttons: [
-                    .destructive(Text("Sign Out"), action: {
-                        print("handle sign out")
-                        vm.handleSignOut()
-                    }),
-                    .cancel()
-                ])
-            }
-            .fullScreenCover(isPresented: $vm.isUserCurrentlyLoggedOut) {
-                LoginView(didCompleteLoginProcess: {
-                    vm.isUserCurrentlyLoggedOut = false
-                    vm.fetchCurrentUser()
-                })
-            }
-        }
-
         var body: some View {
             NavigationView {
 
                 VStack {
-                    customNavBar
-                    messagesView
+                    CustomNavBar(vm: vm, shouldShowLogOutOptions: $shouldShowLogOutOptions)
+                    MessagesView()
                 }
                 .overlay(
                     newMessageButton, alignment: .bottom)
                 .navigationBarHidden(true)
-            }
-        }
-
-        private var messagesView: some View {
-            ScrollView {
-                ForEach(0..<10, id: \.self) { num in
-                    VStack {
-                        HStack(spacing: 16) {
-                            Image(systemName: "person.fill")
-                                .font(.system(size: 32))
-                                .padding(8)
-                                .overlay(RoundedRectangle(cornerRadius: 44)
-                                            .stroke(Color(.label), lineWidth: 1)
-                                )
-
-
-                            VStack(alignment: .leading) {
-                                Text("Username")
-                                    .font(.system(size: 16, weight: .bold))
-                                Text("Message sent to user")
-                                    .font(.system(size: 14))
-                                    .foregroundColor(Color(.lightGray))
-                            }
-                            Spacer()
-
-                            Text("22d")
-                                .font(.system(size: 14, weight: .semibold))
-                        }
-                        Divider()
-                            .padding(.vertical, 8)
-                    }.padding(.horizontal)
-
-                }.padding(.bottom, 50)
             }
         }
 
@@ -181,7 +43,106 @@ struct MainMessagesView: View {
                     .shadow(radius: 15)
             }
         }
+}
 
+struct CustomNavBar: View {
+    
+    @ObservedObject var vm: MainMessagesViewModel
+    @Binding var shouldShowLogOutOptions: Bool
+    
+    var body: some View{
+        HStack(spacing: 16) {
+            AsyncImage(url: URL(string: vm.chatUser?.profileImageUrl ?? "")) { returnedImage in
+                returnedImage
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 50, height: 50)
+                    .clipped()
+                    .cornerRadius(50)
+                    .overlay(RoundedRectangle(cornerRadius: 44)
+                        .stroke(Color(.label), lineWidth: 1)
+                    )
+                    .shadow(radius: 5)
+            } placeholder: {
+                ProgressView()
+            }
+            
+            VStack(alignment: .leading, spacing: 4) {
+                Text(vm.chatUser?.email.replacingOccurrences(of: "@gmail.com", with: "") ?? "")
+                    .font(.system(size: 24, weight: .bold))
+
+                HStack {
+                    Circle()
+                        .foregroundColor(.green)
+                        .frame(width: 14, height: 14)
+                    Text("online")
+                        .font(.system(size: 12))
+                        .foregroundColor(Color(.lightGray))
+                }
+
+            }
+
+            Spacer()
+            Button {
+                shouldShowLogOutOptions.toggle()
+            } label: {
+                Image(systemName: "gear")
+                    .font(.system(size: 24, weight: .bold))
+                    .foregroundColor(Color(.label))
+            }
+        }
+        .padding()
+        .actionSheet(isPresented: $shouldShowLogOutOptions) {
+            .init(title: Text("Settings"), message: Text("What do you want to do?"), buttons: [
+                .destructive(Text("Sign Out"), action: {
+                    print("handle sign out")
+                    vm.handleSignOut()
+                }),
+                .cancel()
+            ])
+        }
+        .fullScreenCover(isPresented: $vm.isUserCurrentlyLoggedOut) {
+            LoginView(didCompleteLoginProcess: {
+                vm.isUserCurrentlyLoggedOut = false
+                vm.fetchCurrentUser()
+            })
+        }
+    }
+}
+
+struct MessagesView: View {
+    var body: some View {
+        ScrollView {
+            ForEach(0..<10, id: \.self) { num in
+                VStack {
+                    HStack(spacing: 16) {
+                        Image(systemName: "person.fill")
+                            .font(.system(size: 32))
+                            .padding(8)
+                            .overlay(RoundedRectangle(cornerRadius: 44)
+                                        .stroke(Color(.label), lineWidth: 1)
+                            )
+
+
+                        VStack(alignment: .leading) {
+                            Text("Username")
+                                .font(.system(size: 16, weight: .bold))
+                            Text("Message sent to user")
+                                .font(.system(size: 14))
+                                .foregroundColor(Color(.lightGray))
+                        }
+                        Spacer()
+
+                        Text("22d")
+                            .font(.system(size: 14, weight: .semibold))
+                    }
+                    Divider()
+                        .padding(.vertical, 8)
+                }.padding(.horizontal)
+
+            }.padding(.bottom, 50)
+        }
+    }
 }
 
 struct MainMessagesView_Previews: PreviewProvider {
---
+++ Firebase Chat App/Utils/FirebaseManager.swift
@@ -0,0 +1,30 @@
+//
+//  FirebaseManager.swift
+//  Firebase Chat App
+//
+//  Created by Sadman Adib on 21/8/22.
+//
+
+import Foundation
+import Firebase
+import FirebaseStorage
+
+class FirebaseManager: NSObject {
+    
+    let auth: Auth
+    let storage: Storage
+    let firestore: Firestore
+    
+    static let shared = FirebaseManager()
+    
+    override init() {
+        FirebaseApp.configure()
+        
+        self.auth = Auth.auth()
+        self.storage = Storage.storage()
+        self.firestore = Firestore.firestore()
+        
+        super.init()
+    }
+    
+}
